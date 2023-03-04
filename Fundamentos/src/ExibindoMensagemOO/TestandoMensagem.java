c699041373f54af64c2f44ac3660ad79509b0643
Kauanne Oliveira <paulakauanne2411@gmail.com>
2023-03-04 00:02:42 -0300

    Add files via upload

---
+++ Fundamentos/src/ExibindoMensagemOO/TestandoMensagem.java
@@ -0,0 +1,11 @@
+package ExibindoMensagemOO;
+
+public class TestandoMensagem {
+
+		public static void main(String args[]) {
+			
+			ExibindoMensagem objMensagem = new ExibindoMensagem();
+			
+			objMensagem.Mensagem();
+		}
+}
